cac082be05e75fe7feea7deab32bfb381cc1ef8b
Zining <ziningxie10@gmail.com>
2024-04-15 21:52:13 -0400

    first commit

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from './Labs/Lab5.js'
+import cors from 'cors'
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-// import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-import cors from 'cors'
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+
+
 const app = express()
 app.use(cors())
 app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+AssignmentRoutes(app);
+
+
+// import ModuleRoutes from "./Kanbas/modules/routes.js";
+// import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+// import cors from 'cors'
 // AssignmentRoutes(app)
-ModuleRoutes(app)
-CourseRoutes(app)
+// ModuleRoutes(app)
+// CourseRoutes(app)
 Lab5(app)
 Hello(app)
-app.listen(4000)
-// app.listen(process.env.PORT || 4000)
+// app.listen(4000)
+app.listen(process.env.PORT || 4000)
